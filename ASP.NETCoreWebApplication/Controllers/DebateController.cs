26645b9f8224ed83a4c2e8d1f0a26bcefffab10b
Noé DUBOIS <noe@thesaur.io>
2021-12-14 09:28:50 +0100

    API on Debate, react's composant

---
+++ ASP.NETCoreWebApplication/Controllers/DebateController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ASP.NETCoreWebApplication.Data;
+using ASP.NETCoreWebApplication.Models;
+
+namespace ASP.NETCoreWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DebatController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DebatController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Debat
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Debate>>> GetDebate()
+        {
+            return await _context.Debate.ToListAsync();
+        }
+
+        // GET: api/Debat/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Debate>> GetDebate(Guid id)
+        {
+            var debate = await _context.Debate.FindAsync(id);
+
+            if (debate == null)
+            {
+                return NotFound();
+            }
+
+            return debate;
+        }
+
+        // PUT: api/Debat/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDebate(Guid id, Debate debate)
+        {
+            if (id != debate.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(debate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DebateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Debat
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Debate>> PostDebate(Debate debate)
+        {
+            _context.Debate.Add(debate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDebate", new { id = debate.Id }, debate);
+        }
+
+        // DELETE: api/Debat/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDebate(Guid id)
+        {
+            var debate = await _context.Debate.FindAsync(id);
+            if (debate == null)
+            {
+                return NotFound();
+            }
+
+            _context.Debate.Remove(debate);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DebateExists(Guid id)
+        {
+            return _context.Debate.Any(e => e.Id == id);
+        }
+    }
+}
--- ASP.NETCoreWebApplication/Models/Debate.cs
+++ ASP.NETCoreWebApplication/Models/Debate.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using ASP.NETCoreWebApplication.Models.Comments;
 
 namespace ASP.NETCoreWebApplication.Models
 {
     public class Debate
     {
-        public int Id { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string? Genre { get; set; }
 
-        public List<MainComment> MainComments { get; set; }
+        //public List<MainComment>? MainComments { get; set; }
     }
 }
