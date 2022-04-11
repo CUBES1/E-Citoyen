a2d15f406ff463d83d1adf8fe917f0c08cc1fef1
Noé DUBOIS <noe@thesaur.io>
2022-04-11 16:20:52 +0200

    authentification

---
+++ ASP.NETCoreWebApplication/ViewModel/AuthenticateModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NETCoreWebApplication.ViewModel
+{
+    public class AuthenticateModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
